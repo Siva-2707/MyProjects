0111de973e707d5a5924fd42151f41baffef938b
Sivarajesh B <b.sivarajesh20@gmail.com>
2022-03-13 22:45:51 +0530

    Queue started

---
+++ Eclipse/DS & Algorithims/src/com/siva/queue/CircularQueue.java
@@ -0,0 +1,24 @@
+package com.siva.queue;
+
+public class CircularQueue {
+	
+	private int head, tail = -1;
+	private int arr[];
+	private int size;
+	CircularQueue(int size){
+		this.size = size;
+		arr = new int[size];
+	}
+	
+	
+	public void  enqueue(int data) {
+		
+	}
+	public void dequeue() {
+		
+	}
+	public void peek() {
+		
+	}
+
+}
