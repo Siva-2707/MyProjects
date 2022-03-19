4d45d9e598cb68c58c36e566dd80545c5dce5731
Sivarajesh B <b.sivarajesh20@gmail.com>
2022-03-19 19:01:53 +0530

    Design Pattern Project added

---
+++ Eclipse/Design Pattern/src/com/siva/dp/SingleTonDemo.java
@@ -0,0 +1,16 @@
+package com.siva.dp;
+
+public class SingleTonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleTon st = SingleTon.getInstance();
+		st.setValue(34);
+		System.out.println(st.getValue());
+		
+		SingleTon st1 = SingleTon.getInstance();
+		System.out.println(st1.getValue());
+		
+	}
+
+}
